963cd7152bb03cb588bb1a5d93284d8dd509b5ab
yuknak <yuknak4@gmail.com>
2021-03-01 13:06:04 +0900

    Change snn to vnn for ios

--- mobile/ios/vnncliTests/vnncliTests.m
+++ mobile/ios/vnncliTests/vnncliTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface snncliTests : XCTestCase
+@interface vnncliTests : XCTestCase
 
 @end
 
-@implementation snncliTests
+@implementation vnncliTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
